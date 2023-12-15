10ab2586b45bb5e878f055a6dde1e472a84e39e3
sebswit <sebastian.switakowski@gmail.com>
2023-12-15 19:34:24 +0000

    pseudo code

---
+++ script.js
@@ -0,0 +1,44 @@
+
+// create a variable to store the city name
+// create a variable to store API key
+const apiKey = "";
+// create a variable to store the queryURL
+var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + apiKey;
+// create a variable to store the search history
+// create a variable to store the current date
+   // use a data.js 
+// create a variable to store the current weather
+// create a variable to store the forecast
+// create a variable to store the humidity
+// create a variable to store the wind speed
+
+// Function to get data from openweather
+
+// get data from form
+    // validate form
+    // function to validate form 
+    // clear form
+    //
+// fetch data from openweather
+   // fetch(queryURL)
+   // parse data
+   // display data
+// update HTML
+  // update current weather
+    // update date
+    // update city name
+    // update 5 day forecast
+    // update search history
+    // update humidity
+    // update wind speed
+ // update forecast
+    // update date
+    // update icon
+    // update temp
+    // update humidity
+ // save to local storage
+   // check if city is in local storage
+    // if not, add city to local storage
+    // if yes, do nothing
+ // get data from local storage
+    // display data
