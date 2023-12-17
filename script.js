59aeb0c3d03aa9989ee414e4a1123bfb4f54e974
sebswit <sebastian.switakowski@gmail.com>
2023-12-17 08:16:19 +0000

    event listener changed, function to create html

--- script.js
+++ script.js
@@ -1,83 +1,36 @@
 
-
+//var city = document.getElementById("search-input").value;
 // create a variable to store the city name
-var citySearch = $("#search-input").val();
+ 
 // create a variable to store API key
-const apiKey = "";
-// create a variable to store the queryURL
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=lat=" + lat + "lon=" + lon + "&appid=" + apiKey;
-var queryURLforecast = "http://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&appid=" + apiKey;
-// create a variable to store the search history
-var searchHistory = [];
+const APIKey = "00be002f543021f3f037894c47ccc2ac";
+// Here we are building the URL we need to query the database
+//var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+APIKey;
 
-   // use a data.js 
+$('#search-button').on('click', function(event){
+   event.preventDefault();
+  var city = document.getElementById("search-input").value;
+  console.log(city);
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +city+ "&appid=" +APIKey;
 
-// event listener for search button
-$("#search-button").on("click", function(event) {
-  event.preventDefault();
-  // get the city name from the input field
-  var city = $("#search-input").val();
-  // add the city name to the search history
-  searchHistory.push(city);
-  // save the search history to local storage
-  localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
-  // call the getWeather function
-  getWeather();
-});
-
-//convert city name to lat and lon
-function getLocation(city) {
-   if (city.geolocation) {
-     city.geolocation.watchPosition(showPosition);
-   } else { 
-     x.innerHTML = "Geolocation is not supported by this browser.";
-   }
- }
-     
- function showPosition(position) {
-   var lat = position.coords.latitude;
-   var lon = position.coords.longitude;
- }
-// Function to get data from openweather for current weather
- fetch(queryURL)
-  .then(function (response) {
+  fetch(queryURL).then(function(response){
     return response.json();
-  })
-  .then(function (data) {
-    // city name
-    var cityName = data.name;  
-    // date
-    var currentDate = data.dt;
-    // temperature
-    var temp = data.main.temp;
-    //humidity
-    var humidity = data.main.humidity;  
-      // wind speed
-    var windSpeed = data.wind.speed;
-      // weather icon
-    var icon = data.weather[0].icon; 
+  }).then(function(data){ 
+    console.log(data);
+    $('#today').text(JSON.stringify(data.name));
   });
-
-// Function to get data from openweather for 5 day forecast
-fetch(queryURLforecast)
-   .then(function (response) {
-      return response.json();
-   })
-   .then(function (data) {
-      // date
-      var date = data.list[0].dt_txt;
-      // temperature
-      var temp = data.list[0].main.temp;
-      //humidity
-      var humidity = data.list[0].main.humidity;  
-         // weather icon
-      var icon = data.list[0].weather[0].icon; 
-   });
-// function to display data on page
-function displayWeather() {
-   // create card for current wather
-
-   // create card for 5 day forecast
-  
-// 
+});
+// Function to handle and update HTML with current weather data
+function handleCurrentWeather(data) {
+  const todaySection = document.getElementById('today');
+  todaySection.innerHTML = `
+    <h3>Today's Weather</h3>
+    <p>City: ${data.name}</p>
+    <p>Date: ${new Date(data.dt * 1000).toLocaleDateString()}</p>
+    <p>Temperature: ${data.main.temp} °C</p>
+    <p>Humidity: ${data.main.humidity}%</p>
+    <p>Wind Speed: ${data.wind.speed} m/s</p>
+  `;
 }
+
+
